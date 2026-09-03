b07d230285544e475a86e4bbae924838fc120aa6
Cedriman <senzocharlie7@gmail.com>
2026-09-03 04:30:36 +0200

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,34 @@
+// assets/js/data.js
+const AR_MEDIA_DATABASE = {
+    // Media assets tied specifically to Target Index 0
+    "0": {
+        videos: [
+            {
+                src: "assets/video/effv3.mp4",
+                title: "Target 0 - Video Showcase A"
+            },
+            {
+                src: "assets/video/effv1.mp4",
+                title: "Target 0 - Video Showcase B"
+            }
+        ]
+    },
+    
+    // Media assets tied specifically to Target Index 3
+    "3": {
+        videos: [
+            {
+                src: "assets/video/effv3.mp4",
+                title: "EFF Video 1"
+            },
+            {
+                src: "assets/video/effv1.mp4",
+                title: "EFF Video 2"
+            },
+            {
+                src: "assets/video/effv2.mp4",
+                title: "EFF Video 3"
+            }
+        ]
+    }
+};
